d0a6b28c1008c1e86a007510c9ff974cb06444b7
evanson <igiri.evanson@gmail.com>
2021-03-28 20:22:42 +0100

    finshed the expense tracker app

--- src/components/AddTransaction.js
+++ src/components/AddTransaction.js
@@ -1,20 +1,47 @@
-import React from 'react'
+import React, { useState, useContext } from 'react';
+import { GlobalContext } from '../context/GlobalState';
 
 const AddTransaction = () => {
+  const [text, setText] = useState('');
+  const [amount, setAmount] = useState(0);
+
+  const { addTransaction } = useContext(GlobalContext);
+
+  const onSubmit = e => {
+    e.preventDefault();
+    const newTransaction = {
+      id: Math.floor(Math.random() * 100000000),
+      text,
+      amount: +amount
+    }
+    addTransaction(newTransaction);
+    setAmount(0);
+    setText('');
+  }
+
   return (
     <>
       <h3>Add new transaction</h3>
-      <form>
+      <form onSubmit={onSubmit}>
         <div className="form-control">
-          <label HtmlFor="text">Text</label>
-          <input type="text" placeholder="Enter text..." />
+          <label htmlFor="text">Text</label>
+          <input 
+            type="text" 
+            placeholder="Enter text..." 
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+            />
         </div>
         <div className="form-control">
-          <label HtmlFor="amount"
+          <label htmlFor="amount"
             >Amount <br />
             (negative - expense, positive - income)</label
           >
-          <input type="number" placeholder="Enter amount..." />
+          <input 
+            type="number" 
+            placeholder="Enter amount..."
+            value={amount}
+            onChange={(e) => setAmount(e.target.value)} />
         </div>
         <button className="btn">Add transaction</button>
       </form>
